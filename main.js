01dc13e256a91836923c2bed80322e67918f4ed3
kunal90803 <palk27530@gmail.com>
2025-02-01 01:07:08 +0530

    ui_update

---
+++ main.js
@@ -0,0 +1,35 @@
+// let mailser=document.querySelector("input");
+let button=document.querySelector(".subscribe");
+let error=document.querySelector(".error");
+let success=document.querySelector(".success");
+let dismissBtn=document.querySelector(".dismiss");
+let mailid=document.querySelector(".mailid");
+button.addEventListener("click",function(){
+    let email=mail.value;
+    console.log(email);
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(pattern.test(email)){
+        // alert("Email is valid");
+        error.classList.add("hide")
+        mailid.innerHTML=email;
+        document.querySelector(".container").style.display="block";
+        document.querySelector("section").classList.add("hide");
+
+
+        
+    }
+    else{
+        error.classList.remove("hide")
+
+    }
+
+})
+dismiss.addEventListener("click",()=>{
+    
+    document.querySelector(".container").style.display="none";
+    document.querySelector("section").classList.remove("hide");
+    mail.value="";
+
+
+})
